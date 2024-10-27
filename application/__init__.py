f9a8fa04d5bcf29a7f99cf9845b367d3afc90251
Mikhail Belskiy <mikl.belskiy@yandex.ru>
2024-10-27 15:44:57 +0300

    Модули, пакеты, импорты в Python

---
+++ application/__init__.py
@@ -0,0 +1,4 @@
+import datetime
+current_datetime = datetime.datetime.now()
+print(f'Тукущие дата и время {current_datetime}')
+
---
+++ application/salary.py
@@ -0,0 +1,2 @@
+def calculate_salary():
+    return "Зарабатная плата 5 числа каждого месяца"
